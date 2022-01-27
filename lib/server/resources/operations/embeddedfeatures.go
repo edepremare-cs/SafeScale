57d41cb4cb26c2042b006ecffdada0457e964724
Vincent Planchenault <vincent.planchenault@csgroup.eu>
2022-01-27 14:58:12 +0100

    Splitted Feature ansible to ansible and ansible-for-clustezr
    - ansible adds ansible binary and minimal configuration
    - ansible-for-cluster adds dynamic inventory

--- lib/server/resources/operations/embeddedfeatures.go
+++ lib/server/resources/operations/embeddedfeatures.go
@@ -173,14 +173,14 @@ func ansibleFeature() *Feature {
 }
 
 // ansibleForClusterFeature  ...
-func ansibleForClusterFeature() *Feature {
+func ansibleForClusterFeature() *FeatureFile {
 	name := "ansible-for-cluster"
 	filename, specs, err := loadSpecFile(name)
 	err = debug.InjectPlannedError(err)
 	if err != nil {
 		panic(err.Error())
 	}
-	return &Feature{
+	return &FeatureFile{
 		displayName: name,
 		fileName:    filename,
 		embedded:    true,
